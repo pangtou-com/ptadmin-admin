3976b20ef25ed8ed32be86b0b91dcf1ed07e2fb3
Zane <873934580@qq.com>
2024-09-19 16:53:13 +0800

    fix: Update version to v0.0.5

--- public/index.php
+++ public/index.php
@@ -25,7 +25,7 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('PTADMIN_START', microtime(true));
-const PTADMIN_FRAME_VERSION = 'v0.0.4';
+const PTADMIN_FRAME_VERSION = 'v0.0.5';
 
 if (version_compare(PHP_VERSION, '7.3', '<')) {
     $version = PHP_VERSION;
